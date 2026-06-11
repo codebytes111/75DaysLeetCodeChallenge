7eb7ac06c7e4e735f09a96f833dac3817057aacf
codebytes111 <codebytes111@gmail.com>
2026-06-11 21:20:52 +0530

    Time: 61 ms (5.01%), Space: 48.6 MB (9.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int ans[] = new int[k];
-        List<Integer>[] bucket = new List[nums.length+1];
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
-            }
-            else{
-            map.put(nums[i],map.get(nums[i])+1);
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            int fre = 0;
+            int num = 0;
+        for(int key : map.keySet()){
+            if(map.get(key)>fre){
+                fre = map.get(key);
+                num=key;
             }
         }
-            for(int key : map.keySet()){
-                int freq = map.get(key);
-
-                if(bucket[freq] == null){
-                    bucket[freq] = new ArrayList<>();
-                }
-                bucket[freq].add(key);
-            }
-        int index = 0;
-        for(int i=bucket.length-1; i>0 && index < k; i--){
-            if(bucket[i]!= null){
-                for(int num : bucket[i]){
-                    ans[index++]=num;
-                    if(index == k){
-                        break;
-                    }
-                }
-            }
-        }
-         return ans;   
+        result[i] = num;
+        map.remove(num);
     }
-} 
+    return result;
+    }
+}
