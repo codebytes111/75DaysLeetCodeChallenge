8bdc1fd1df8ef2394b14ea26e744f2f280f967fc
codebytes111 <codebytes111@gmail.com>
2026-07-17 03:51:49 +0530

    Time: 14 ms (92.39%), Space: 51.4 MB (74.82%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+
+        while(i<s.length && j<g.length){
+            if(s[i]>=g[j]){
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+}
