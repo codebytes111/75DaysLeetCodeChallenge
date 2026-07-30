cec9fccf9a0bc293f220df2e427e10ee6b166218
codebytes111 <codebytes111@gmail.com>
2026-07-30 22:49:20 +0530

    Time: 3 ms (11.24%), Space: 43.5 MB (16.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int len = word.length();
+        int count[] = new int[26];
+        for(char ch : word.toCharArray()){
+            count[ch-'a']++;
+        }
+        Arrays.sort(count);
+        int total = 0;
+        int push = 1;
+        int used = 0;
+        for(int i=25;i>=0;i--){
+            if(count[i]==0) break;
+            total+=count[i] * push;
+            used++;
+            if(used==8) push = 2;
+            if(used==16) push = 3;
+            if(used==24) push = 4;
+        }
+        
+        return total;
+    }
+}
